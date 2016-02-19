eee52d56903f52eef404043a49f205997ce23769
Uğur Erdal <ugur@polisoft.com.tr>
2016-02-19 11:25:45 +0200

    user post get decleration in project

--- app.js
+++ app.js
@@ -44,12 +44,16 @@ app.use('/blobs', blobs);
 app.use('/vehicles', vehicles);
 app.use('/users', users);
 
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
+
+
 
 // error handlers
 
--- routes/users.js
+++ routes/users.js
@@ -2,33 +2,83 @@
 var express = require('express'),
     router = express.Router(),
     mongoose = require('mongoose'), //mongo connection
-    morgan      = require('morgan');
-    jwt         = require('jsonwebtoken'); // token
-    bodyParser = require('body-parser'); //parses information from POST
+    morgan      = require('morgan'),
+    jwt         = require('jsonwebtoken'), // token
+    bodyParser = require('body-parser'), //parses information from POST
     methodOverride = require('method-override'); //used to manipulate POST
 
-//basic route
-router.get('/', function (req, res) {
-    res.send('MERHABA ! fueldroid user api bölümündesiniz');
+var User = require('../model/users');
 
+router.use(bodyParser.urlencoded({ extended: true }))
+router.use(methodOverride(function(req, res){
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method
+        delete req.body._method
+        return method
+      }
+}))
+
+
+router.route('/')
+//get all users
+    .get(function (req, res, next) {
+
+    	mongoose.model('User').find({}, function (err, users) {
+    		if (err) {
+    			return console.error(err);
+    		} else {
+    			res.format({
+    				json:function() {
+    					res.json(users);
+    				}
+    			});
+    		}
+    	});
+    })
+
+    .post(function(req, res) {
+    	mongoose.model('User').create({
+    		name: 'admin',
+    		password: 'admin',
+    		admin: 'true'
+    }, function (err, user) {
+    	if (err) {
+    		res.send("database de bi sıgındı var");
+    	} else {
+    		console.log('users data' + user);
+    		res.format({
+    			json:function(){
+    				res.json(user);
+    			}
+
+    		});
+    	}
+    })
 });
+
+
+
+
+
+
+
 
 //not basic route first user register
 router.get('/setup', function(req, res) {
 
-	var nick = new User({
-	    name: 'ugur',
-		password: 'erdal',
-		admin: 'true'
-	});
-	nick.save(function (err) {
-		if (err) throw err;
+    var nick = new user({
+        name: 'ugur',
+        password: 'erdal',
+        admin: 'true'
+    });
+    nick.save(function (err) {
+        if (err) throw err;
 
-		console.log('User saved succesfully');
-		res.json({ succes:true });
-	});
+        console.log('User saved succesfully');
+        res.json({ succes:true });
+    });
 });
-
 
 
 module.exports = router;
